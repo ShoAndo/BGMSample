392d4343b305a2484bcbc3424844181d467f801f
Sho Ando <spdtas0223@gmail.com>
2019-08-19 10:12:47 +0800

    final?

--- BGMSample/ViewController.swift
+++ BGMSample/ViewController.swift
@@ -9,13 +9,78 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    
+//    var i = 0
+//    カセットデッキ的なやつ
+    var player: AVAudioPlayer!
+    
+//    var isPlaying = true
 
+    @IBOutlet weak var button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        再生する音楽ファイルのパス作成,テンプレ
+        //        forresource:音楽のファイルの名前
+        
+        //        ofType:ファイルの形式
+        let audioPath = Bundle.main.path(forResource: "bgm1", ofType: "mp3")!
+        let audioUrl = URL(fileURLWithPath: audioPath)
+        
+//        音楽ファイルをもとに、プレイヤー作成
+//        do-catchが必要な分はエラーが出る
+        do{
+            player = try AVAudioPlayer(contentsOf: audioUrl)
+//            無限るーぷ
+            player.numberOfLoops = -1
+        }catch let error{
+            print(error.localizedDescription)
+        }
+//        再生
+        player.delegate = self //おまじない
+        player.prepareToPlay() //再生の準備
+        player.play()          //再生
+        
+        
     }
 
+   @IBAction func didClickButton(_ sender: Any) {
+ /*       i += 1
+        
+        if i == 1{
+            button.
+            player.pause()
+        }else if i == 2{
+            i = 0
+            player.play()
+           
+        }
+ */
+    
+    /*
+     if isPlaying{
+        player.pause()
+     }else{
+        player.play()
+     }
+     
+     isPlaying = !isPlaying
+     
+     }
+ */
+    
+    if player.isPlaying{
+        player.pause()
+        button.setTitle("再生", for: .normal)
+    }else{
+        player.play()
+        button.setTitle("停止", for: .normal)
+        
+    }
+      
+    }
 
+    
 }
 
